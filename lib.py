1969c03936da0b2058aea399a1715ab18e8b7628
SamHendry <134175998+SamHendry@users.noreply.github.com>
2023-10-16 15:30:44 -0700

    removed a debug line

--- lib.py
+++ lib.py
@@ -170,7 +170,6 @@ def tt_display(tasks):
 
     # display
     header = [' ', 'name']; header.extend(tasks[list(tasks.keys())[0]].keys())
-    print(header)
     tt.print(
         data, 
         header=header, 
